126c92d9edd1b78b50900a1ca9f64ccc3f68ff99
ARTHUR LIMA TRAUTWEIN <25203614@CFP127ALUNO.LOCAL>
2025-10-27 11:30:39 -0300

    concluido

--- src/br/senai/sp/jandira/media_final/MediaFinalApp.java
+++ src/br/senai/sp/jandira/media_final/MediaFinalApp.java
@@ -3,14 +3,13 @@ package br.senai.sp.jandira.media_final;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.util.Objects;
+import java.util.Optional;
+
 
 public class MediaFinalApp extends Application {
 
@@ -62,7 +61,7 @@ public class MediaFinalApp extends Application {
         // Painel de formulários
         VBox painelFormulario = new VBox();
         painelFormulario.setPadding(new Insets(0, 0, 10, 10));
-        Label labelNome = new Label("Nome do aluno: ");
+        Label labelNome = new Label("Nome do Aluno: ");
         Label labelNota1 = new Label("Nota1: ");
         Label labelNota2 = new Label("Nota2: ");
         Label labelNota3 = new Label("Nota3: ");
@@ -93,7 +92,77 @@ public class MediaFinalApp extends Application {
         stage.show();
 
         // Eventos de clique dos botões
-        // buttonCalcularMedia.addEventHandler();
+        buttonCalcularMedia.setOnAction(click ->{
+            System.out.println("Botão do Brayan clicado!");
+            String nomeDigitado =textFieldNome.getText();
+            labelAluno.setText("Nome do Aluno: " + nomeDigitado);
 
+            // CALCULAR MÉDIA
+            // OBTER AAS NOTAS
+
+            //Criar um vetor (array) de notas
+            double notas[] = new double[4];
+            String notasStr[] = new String[4];
+
+            notasStr[0] = textFieldNota1.getText();
+            notas[0] = Double.parseDouble(notasStr[0]);
+
+            notasStr[1] = textFieldNota2.getText();
+            notas[1] = Double.parseDouble(notasStr[1]);
+
+            notasStr[2] = textFieldNota3.getText();
+            notas[2] = Double.parseDouble(notasStr[2]);
+
+            notasStr[3] = textFieldNota4.getText();
+            notas[3] = Double.parseDouble(notasStr[3]);
+
+            // USO DE LOOP while (ENQUANTO)
+            double mediaFinal = 0.0;
+            int i =0;
+            while (i < notas.length){
+                mediaFinal = mediaFinal + notas[i];
+                i = i + 1;
+            }
+
+            mediaFinal = mediaFinal / notas.length;
+            // double mediaFinal = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+            String mediaFinalStr = String.format("%.2f", mediaFinal);
+
+            labelMediaFinal.setText("Média Final: " + mediaFinalStr);
+
+            String situacao;
+            if (mediaFinal >= 6) {
+                situacao = "Aprovado";
+            } else if (mediaFinal < 4) {
+                situacao = "Reprovado";
+            } else {
+                situacao = "Recuperação";
+            }
+
+            labelSituacao.setText("Situação: " + situacao);
+
+        });
+
+        buttonLimpar.setOnAction(click ->{
+            textFieldNome.clear();
+            textFieldNota1.clear();
+            textFieldNota2.clear();
+            textFieldNota3.clear();
+            textFieldNota4.clear();
+            labelMediaFinal.setText("Média Final:");
+            labelSituacao.setText("Situação: ");
+            labelAluno.setText("Nome do Aluno: ");
+            textFieldNome.requestFocus();
+        });
+        buttonSair.setOnAction(click ->{
+            Alert alerta = new Alert(Alert.AlertType.CONFIRMATION, "Fechar aplicação", ButtonType.YES, ButtonType.NO);
+            Optional<ButtonType> botaoPressionado = alerta.showAndWait();
+
+            if (botaoPressionado.get() == ButtonType.YES){
+                Alert alerta2 = new Alert(Alert.AlertType.INFORMATION, "Até logo grupo");
+                alerta2.showAndWait();
+                System.exit(0);
+            }
+        });
     }
 }
